3505230b21fe30caffaedd00829a12fca63152be
plaguer <101288516+VillainsRule@users.noreply.github.com>
2024-03-02 23:46:36 -0500

    perm solution

--- blooks/sellDupeBlooks.js
+++ blooks/sellDupeBlooks.js
@@ -1,4 +1,8 @@
 (async () => {
+    let res = await fetch('/lib/js/blooks.js');
+    res = await res.text();
+    let endpoint = res.match(/requests.post\("(.*?)",/)[1];
+    
     if (!blacket.blooks) return alert(`You must be on the Market or Blooks pages to run this script.`);
     if (!confirm('Do you actually want to sell all of your duplicate blooks?')) return alert(`Cancelled script.`);
 
@@ -14,7 +18,7 @@
 
     let sellInterval = setInterval(() => {
         if (Object.keys(pendingSale).length == 0) return clearInterval(sellInterval);
-        blacket.requests.post('/worker2/sell', {
+        blacket.requests.post(endpoint, {
             blook: Object.keys(pendingSale)[0],
             quantity: Object.values(pendingSale)[0]
         }, (res) => {
